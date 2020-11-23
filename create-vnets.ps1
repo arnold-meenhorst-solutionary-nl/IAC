c1f8193940fbc230cb549f14325fcdf320fb2d12
arnold-meenhorst-solutionary-nl <arnold.meenhorst@gmail.com>
2020-11-23 18:19:37 +0100

    Update create-vnets.ps1

--- create-vnets.ps1
+++ create-vnets.ps1
@@ -2,15 +2,14 @@ $location = 'westeurope'
 $resourceGroup = 'rg-pg-connectivity'
 $rdpRule              = New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
 $networkSecurityGroup = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location -Name "nsg-pg-hub" -SecurityRules $rdpRule
-$frontendSubnet       = New-AzVirtualNetworkSubnetConfig -Name frontendSubnet -AddressPrefix "10.0.1.0/24" -NetworkSecurityGroup $networkSecurityGroup
-$backendSubnet        = New-AzVirtualNetworkSubnetConfig -Name backendSubnet  -AddressPrefix "10.0.2.0/24" -NetworkSecurityGroup $networkSecurityGroup
-New-AzVirtualNetwork -Name vnet-pg-hub -ResourceGroupName $resourceGroup -Location $location -AddressPrefix "10.0.0.0/16" -Subnet $frontendSubnet,$backendSubnet
+$frontendSubnet       = New-AzVirtualNetworkSubnetConfig -Name frontendSubnet -AddressPrefix "10.1.1.0/24" -NetworkSecurityGroup $networkSecurityGroup
+$backendSubnet        = New-AzVirtualNetworkSubnetConfig -Name backendSubnet  -AddressPrefix "10.1.2.0/24" -NetworkSecurityGroup $networkSecurityGroup
+New-AzVirtualNetwork -Name vnet-pg-hub -ResourceGroupName $resourceGroup -Location $location -AddressPrefix "10.1.0.0/16" -Subnet $frontendSubnet,$backendSubnet
 # Naam is niet consequent
 
-$location = 'westeurope'
 $resourceGroup = 'rg-pg-identity-hub'
 $rdpRule              = New-AzNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
 $networkSecurityGroup = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -Location $location -Name "nsg-pg-identity" -SecurityRules $rdpRule
-$frontendSubnet       = New-AzVirtualNetworkSubnetConfig -Name frontendSubnet -AddressPrefix "10.1.1.0/24" -NetworkSecurityGroup $networkSecurityGroup
-$backendSubnet        = New-AzVirtualNetworkSubnetConfig -Name backendSubnet  -AddressPrefix "10.1.2.0/24" -NetworkSecurityGroup $networkSecurityGroup
-New-AzVirtualNetwork -Name vnet-pg-identity -ResourceGroupName $resourceGroup -Location $location -AddressPrefix "10.1.0.0/16" -Subnet $frontendSubnet,$backendSubnet
+$frontendSubnet       = New-AzVirtualNetworkSubnetConfig -Name frontendSubnet -AddressPrefix "10.2.1.0/24" -NetworkSecurityGroup $networkSecurityGroup
+$backendSubnet        = New-AzVirtualNetworkSubnetConfig -Name backendSubnet  -AddressPrefix "10.2.2.0/24" -NetworkSecurityGroup $networkSecurityGroup
+New-AzVirtualNetwork -Name vnet-pg-identity -ResourceGroupName $resourceGroup -Location $location -AddressPrefix "10.2.0.0/16" -Subnet $frontendSubnet,$backendSubnet
