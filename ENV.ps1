d46362a125b7fefc84019399715a48306e6fd7cf
arnold-meenhorst-solutionary-nl <arnold.meenhorst@gmail.com>
2020-11-26 23:07:05 +0100

    required

--- ENV.ps1
+++ ENV.ps1
@@ -1,5 +1,8 @@
 # global default location
 $location='westeurope'
 
-# generate random 4 digit number 
-$random=$(Get-Random -Maximum 10000)
+# wrapper to generate random 4 digit number 
+
+function random {
+   Write-Output $(Get-Random -Maximum 10000)
+}
